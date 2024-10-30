fdab6c0dff90729ba6932b34e3eb69dcec687c43
Philip Venkov <philip.venkov@gmail.com>
2024-10-30 12:09:30 -0700

    exclusion mode with probability reset (strobe)

--- sketch.js
+++ sketch.js
@@ -1,23 +1,48 @@
 let cg;
 
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // calculateDimensions();
   setAttributes('antialias', true);
   canvas.imageSmoothingEnabled = false;
-  cg = createGraphics(1, 1, WEBGL);
+  cg = createGraphics(5, 5);
   cg.noSmooth();
   cg.strokeWeight(0);
-  cg.fill(0);
-  cg.square(0, 0, 1);
+  cg.fill(255,125,0);
+  cg.square(0,0, 1);
   // this._renderer.getTexture(cg).setInterpolation(NEAREST, NEAREST);
-  background(220);
+  background(255);
+  frameRate(24);
+
 }
 
 function draw() {
+let prob = random (0,1);
 
-  
-  imageMode(CENTER);
-  image(cg, mouseX, mouseY, 200, 200);
+
+if (prob < .75) {
+  canvas.imageSmoothingEnabled = false;
+}
+else {
+  canvas.imageSmoothingEnabled = true;
+}
+
+let blockCount = random(1,13);
+  blendMode(EXCLUSION);
+    let scatterY = random(mouseY-100, mouseY+100);
+    let scatterX = random(mouseX-100, mouseX+100);
+    let scaleBlock = random (200,800);
+    image(cg, scatterX, scatterY, scaleBlock, scaleBlock);
+  blendMode(BLEND);
+  if (prob < .025) {
+    background(255);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  calculateDimensions();
 }
 
