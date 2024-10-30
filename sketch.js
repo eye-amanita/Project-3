fd9284001df0d7189d5a405217b2a93d64010e3e
Philip Venkov <philip.venkov@gmail.com>
2024-10-30 16:40:37 -0700

    mouseMoving different behavior from draw no mouse move

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let cg;
 let pause = false;
+let mouseMoving = false;
+let timer = 0;
 
 
 
@@ -22,18 +24,19 @@ function setup() {
 function draw() {
 let prob = random (0,1);
 
-
-let blockCount = random(1,13);
+if (timer == 2) {
   blendMode(EXCLUSION);
     let scatterY = random(mouseY-100, mouseY+100);
     let scatterX = random(mouseX-100, mouseX+100);
     let scaleBlock = random (.2,.8);
-    image(cg, scatterX, scatterY, (1.25*scaleBlock)*width, (1.25*scaleBlock)*width);
-  blendMode(BLEND);
+    image(cg, 20, 20, (1.25*scaleBlock)*width, (1.25*scaleBlock)*width);
+} else {
+
+}
+blendMode(BLEND);
     let obscureX = random(0,width);
     let obscureY = random(0,height);
     let obscureScale = random (100,400);
-
     
     if (prob<.5){
       fill(255);
@@ -43,6 +46,27 @@ let blockCount = random(1,13);
 
   if (prob < .025) {
     background(255);
+  }
+  timer = timer+1;
+
+  if (timer > 2){
+    timer = 2;
+  }
+  
+}
+
+function mouseMoved(){
+  timer = 0;
+  if (timer <2){
+    if (pause == false){
+      mouseMoving = true;
+      blendMode(EXCLUSION);
+      let scatterY = random(mouseY-100, mouseY+100);
+      let scatterX = random(mouseX-100, mouseX+100);
+      let scaleBlock = random (.2,.8);
+      image(cg, scatterX, scatterY, (1.25*scaleBlock)*width, (1.25*scaleBlock)*width);
+      timer = timer-1;
+    }
   }
 }
 
