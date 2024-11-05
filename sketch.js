15b0e79fc999aebeaa92cc2c01e77cd43837d7af
Philip Venkov <philip.venkov@gmail.com>
2024-11-04 16:44:10 -0800

    grid walk neutral behavior implemented

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ let timer = 0;
 let x;
 let y;
 
+let xGridWalk;
+let yGridWalk;
+
 
 
 function setup() {
@@ -15,49 +18,80 @@ function setup() {
   canvas.imageSmoothingEnabled = false;
   cg = createGraphics(5, 5);
   // cg.colorMode(HSB);
-  // cg.colorMode(RGB);
   cg.noSmooth();
   cg.strokeWeight(0);
   cg.fill(20,200,50); //Pink RGB
-  cg.fill(255,255,255); //Pink RGB
-  // cg.fill(340,20,100,255); //HSB
+  cg.fill(255,255,255); //Black RGB
+  
   cg.square(0,0, 1);
-  // this._renderer.getTexture(cg).setInterpolation(NEAREST, NEAREST);
-  x = width / 2;
-  y = height / 2;
-  background(255);
-  frameRate(20);
+  
 
+  // x = width / 2;
+  // y = height / 2;
+  background(255);
+  x = 0;
+  y = 0;
+
+  xGridWalk = 0;
+  yGridWalk = 0;
+  frameRate(20);
+  
 
 }
 
 function draw() {
 let prob = random (0,1);
 
+// Grid Walk Neutral Behavior
 if (timer == 2) {
   blendMode(EXCLUSION);
-    let scatterY = random(mouseY-100, mouseY+100);
-    let scatterX = random(mouseX-100, mouseX+100);
-    let scaleBlock = random (.2,.65);
-    image(cg, x, y, (1.25*scaleBlock)*width, (1.25*scaleBlock)*width);
-    const r = floor(random(4));
-  switch (r) {
-    case 0:
-      x = x + scatterX*.05;
-      break;
-    case 1:
-      x = x - scatterX*.05;
-      break;
-    case 2:
-      y = y + scatterY*.1;
-      break;
-    case 3:
-      y = y - scatterY*.1;
-      break;
-  }
-} else {
+    let scaleBlock = .5;
+    
+    image(cg, xGridWalk, yGridWalk, (scaleBlock*width), (scaleBlock*width));
+    
+    xGridWalk = xGridWalk+((scaleBlock*scaleBlock)*scaleBlock*width);
+    
+    if (xGridWalk > width - ((scaleBlock*scaleBlock)*scaleBlock*width)){
+      yGridWalk = yGridWalk + ((scaleBlock*scaleBlock)*scaleBlock*width);
+      xGridWalk = 0;
+    }
+
+    if (yGridWalk > height){
+
+    xGridWalk = 0;
+    yGridWalk = 0;
+    }
 
 }
+
+//Random Walk Neutral Behavior
+
+// if (timer == 2) {
+//   blendMode(EXCLUSION);
+//     let scatterY = random(mouseY-100, mouseY+100);
+//     let scatterX = random(mouseX-100, mouseX+100);
+//     let scaleBlock = random (.2,.65);
+//     image(cg, x, y, (1.25*scaleBlock)*width, (1.25*scaleBlock)*width);
+//     const r = floor(random(4));
+//   switch (r) {
+//     case 0:
+//       x = x + scatterX*.05;
+//       break;
+//     case 1:
+//       x = x - scatterX*.05;
+//       break;
+//     case 2:
+//       y = y + scatterY*.1;
+//       break;
+//     case 3:
+//       y = y - scatterY*.1;
+//       break;
+//   }
+// } else {
+
+// }
+
+
 blendMode(BLEND);
     let obscureX = random(0,width);
     let obscureY = random(0,height);
